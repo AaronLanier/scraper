5bd7a5b471cd56e7fa018ca7cfdc9ac006aee0e1
Aaron Lanier <alanier.william@gmail.com>
2019-10-05 16:14:26 -0400

    finished app using tutorial

--- server.js
+++ server.js
@@ -1,38 +1,32 @@
-var express = require("express");
-var mongoose = require("mongoose");
-var expressHandlebars = require("express-handlebars");
-var bodyParser = require("body-parser");
-
+var express    = require('express'),
+    bodyParser = require('body-parser'),
+    exphbs     = require('express-handlebars'),
+    logger     = require("morgan");
 
 var PORT = process.env.PORT || 3000;
 
 var app = express();
 
-var router = express.Router();
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
-app.use(express.static(__dirname + "/public"));
-
-app.engine("handlebars", expressHandlebars({
+app.set('views', './views')
+app.engine(
+  "handlebars",
+  exphbs({
     defaultLayout: "main"
-}));
+  })
+);
 app.set("view engine", "handlebars");
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-
-app.use(router);
-
-var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-mongoose.connect(db, function(error) {
-    if(error) {
-        console.log(error);
-    }else {
-        console.log("mongoose connection is successful")
-    }
-});
+require('./config/routes')(app)
 
 app.listen(PORT, function() {
-    console.log("Listening on port:" + PORT);
-});
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
